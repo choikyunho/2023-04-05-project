7d0f5df99d9166cf2193c71ac8f7ecdd49c87d4b
choikyunho <rbsgh777@naver.com>
2023-04-05 10:27:30 +0900

    random

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react";
 
 const GameBoard =()=>{
-    return<div className="bg-blue-100 w-full grow "> 게임보드 </div>
+
+    const [randomNum, setRandomNum] = useState(Math.floor(Math.random()*100));
+    const [hint,setHint]=useState("0~100사이의 숫자를 맞춰보세요!");
+
+    useEffect(()=>console.log(randomNum),[randomNum]);
+
+    return (
+    <div className=" w-full grow flex flex-col justify-center items-center">
+    <div className="mb-4 text-xl font-bold">0~100사이의 숫자를 맞춰보세요!</div>
+    <div>
+        <input className="border-2 rounded-lg px-4 py-2 focus:outline-pink-300 shadow-lg" type="text"/>
+        <button className="px-4 py-2 ml-2 rounded-lg border-2 border-pink-300 text-pink-300 shadow-lg"> 확인 </button>
+    </div>
+    </div>
+    );
 }
 
 export default GameBoard;
