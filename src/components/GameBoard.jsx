ddfb14ff2517b2458e2cb1cea948e95222cd3063
choikyunho <rbsgh777@naver.com>
2023-04-05 11:24:53 +0900

    Hint

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -3,19 +3,33 @@ import { useEffect, useState } from "react";
 const GameBoard =()=>{
 
     const [randomNum, setRandomNum] = useState(Math.floor(Math.random()*100));
-    const [choiceNum, setChoiceNum]=useState(100);
+    const [choiceNum, setChoiceNum]=useState("");
     const [hint,setHint]=useState("0~100사이의 숫자를 맞춰보세요!");
+    const onChangChoice=(e)=>{
+        setChoiceNum(e.target.value);
 
-
+    };
+    const onClickCheck=()=>{
+        let checkNum = parseInt(choiceNum);
+        if (isNaN(checkNum)){
+            setHint("숫자를 입력해주세요!")
+            return;
+        }
+        if (0>checkNum || checkNum>99){
+            setHint("숫자를 잘못 입력하셨습니다!")
+            return;
+        }
+    };
 
     useEffect(() => console.log(`랜덤 숫자는 ${randomNum}입니다.`), [randomNum]);
+    useEffect(() => console.log(`입력한 값은 ${choiceNum}입니다.`), [choiceNum]);
 
     return (
     <div className=" w-full grow flex flex-col justify-center items-center">
-    <div className="mb-4 text-xl font-bold">0~100사이의 숫자를 맞춰보세요!</div>
+    <div className="mb-4 text-xl font-bold">{hint}</div>
     <div>
-        <input className="border-2 rounded-lg px-4 py-2 focus:outline-pink-300 shadow-lg" type="text" value={choiceNum}/>
-        <button className="px-4 py-2 ml-2 rounded-lg border-2 border-pink-300 text-pink-300 shadow-lg"> 확인 </button>
+        <input className="border-2 rounded-lg px-4 py-2 focus:outline-pink-300 shadow-lg" type="text" value={choiceNum} onChange={onChangChoice}/>
+        <button onClick={onClickCheck} className="px-4 py-2 ml-2 rounded-lg border-2 border-pink-300 text-pink-300 shadow-lg"> 확인 </button>
     </div>
     </div>
     );
